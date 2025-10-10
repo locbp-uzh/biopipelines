9b927029d620e089b89e053352dbc3574d71062f
gquarg <gianluca.quargnali@chem.uzh.ch>
2025-10-10 10:58:23 +0200

    dynamic bind debug -> release

--- ExamplePipelines/dynamicbind.py
+++ ExamplePipelines/dynamicbind.py
@@ -19,7 +19,7 @@ from PipelineScripts.compound_library import CompoundLibrary
 from PipelineScripts.pdb import PDB
 
 # Create pipeline
-pipeline = Pipeline("Examples","DynamicBind","Test of several input formats for DynamicBind",debug=True)
+pipeline = Pipeline("Examples","DynamicBind","Test of several input formats for DynamicBind")
 
 pipeline.resources(gpu="V100", memory="16GB", time = "1:00:00")
 
