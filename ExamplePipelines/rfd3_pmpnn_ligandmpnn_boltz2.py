f972e61011e7726831ebf444d63fab601d1eb6e1
gquarg <gianluca.quargnali@chem.uzh.ch>
2026-02-08 13:59:17 +0100

    rfd3 example

--- ExamplePipelines/rfd3_pmpnn_ligandmpnn_boltz2.py
+++ ExamplePipelines/rfd3_pmpnn_ligandmpnn_boltz2.py
@@ -33,24 +33,24 @@ with Pipeline(project="Examples",
     amp = Ligand("AMP")
     ap5 = Ligand("AP5")
 
-    adenylate_kinase_boltz = Boltz2(proteins=adenylate_kinase,
-                                    ligands=ap5) # the sequence is also extracted with PDB
+    #adenylate_kinase_boltz = Boltz2(proteins=adenylate_kinase,
+    #                                ligands=ap5) # the sequence is also extracted with PDB
 
-    rfd3 = RFdiffusion3(pdb=adenylate_kinase_boltz, #RFdiffusion3 often needs some PDB cleanup. The easiest solution is to start from a Boltz prediction
-                        ligand='LIG', 
+    rfd3 = RFdiffusion3(pdb=adenylate_kinase, #RFdiffusion3 often needs some PDB cleanup. The easiest solution is to start from a Boltz prediction
+                        ligand_code='AP5', 
                         contig='A1-121,1-10,A170-214', #They have renamed contigs -> contig
                         num_designs=3)
 
     #this generates a table showing for each structure id a pymol selection for residues within and beyond the distance from the ligand
     distances = DistanceSelector(structures=rfd3,
-                                  ligand="LIG",
+                                  ligand="AP5",
                                   distance=5,
                                   restrict_to=rfd3.tables.structures.designed)
     pmpnn = ProteinMPNN(structures=rfd3,
                         num_sequences=2,
                         redesigned=distances.tables.selections.beyond)
     lmpnn = LigandMPNN(structures=rfd3,
-                       ligand="LIG", #in ligand mpnn you should always specify the ligand name.
+                       ligand="AP5", #in ligand mpnn you should always specify the ligand name.
                        num_sequences=2,
                        redesigned=distances.tables.selections.within)
 
