f21e098021764fa97f870fb59931e4a4166e6a7a
GQChem <gianluca.quargnali@gmail.com>
2026-02-13 11:59:58 +0100

    draft kinase formatting

--- ExamplePipelines/draft_kinase.py
+++ ExamplePipelines/draft_kinase.py
@@ -14,26 +14,23 @@ from biopipelines.panda import Panda
 
 with Pipeline(project="AdenylateKinase", job="LID_Redesign"):
     Resources(gpu="A100", time="4:00:00", memory="16GB")
-
     kinase = PDB("4AKE")
-
     backbones = RFdiffusion(pdb=kinase,
                             contigs='A1-117/50-70/A161-214', 
                             num_designs=10)
-
     sequences = ProteinMPNN(structures=backbones,
                             num_sequences=2,
                             redesigned=backbones.tables.structures.designed)
-
     refolded = AlphaFold(proteins=sequences)    
-    
     conf_change = ConformationalChange(reference_structures = kinase,
                                        target_structures = refolded)
-
-    top3 = Panda(tables=[refolded.tables.confidence,conf_change.tables.changes],
+    top3 = Panda(tables=[refolded.tables.confidence,
+                         conf_change.tables.changes],
                  operations=[Panda.merge(),
                              Panda.filter("RMSD < 1.5 and plddt > 80"),
                              Panda.sort("plddt"),
                              Panda.head(3)],
                  pool=refolded)
+    
+    
 
