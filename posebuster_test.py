cd4602634f8d43850975393d1425a0b363916c62
GQChem <gianluca.quargnali@gmail.com>
2026-02-20 14:39:00 +0100

    remove test

--- posebuster_test.py
+++
@@ -1,8 +0,0 @@
-from biopipelines.pipeline import *
-from biopipelines.load import Load
-from biopipelines.posebusters import PoseBusters
-
-with Pipeline("Test","PoseBusters"):
-    boltz_predictions = Load("/shares/locbp.chem.uzh/gquarg/BioPipelines/Imatinib/PoseSensitivityGnina_002/005_Boltz2")
-    posebusters = PoseBusters(structures=boltz_predictions,
-                              ligand="LIG")
