bc4e79205247d4ec224eefc2c6ee141e4dbca467
GQChem <gianluca.quargnali@gmail.com>
2026-02-21 17:53:03 +0100

    ubiquitin test

--- ExamplePipelines/draft_ubiquitin.py
+++ ExamplePipelines/draft_ubiquitin.py
@@ -3,6 +3,9 @@
 # Licensed under the MIT License. See LICENSE file in the project root for details.
 
 # tested: 17.2.2026
+# pubs.acs.org/doi/10.1021/jacs.5c19875
+# 4LCD: add a keep parameter e.g. chain A and resi 100-150 keep chain E
+# Conformational change alignment add option CA for resi pymol selection (is it standardized?)
 
 from biopipelines.pipeline import *
 from biopipelines.protein_mpnn import ProteinMPNN
@@ -11,17 +14,17 @@ from biopipelines.conformational_change import ConformationalChange
 from biopipelines.panda import Panda
 from biopipelines.dna_encoder import DNAEncoder
 
+
 with Pipeline(project="CAII", job="InverseFolding"):
     Resources(gpu="A100", time="4:00:00", memory="16GB")
-    caII = PDB("3KS3")
-    sequences = ProteinMPNN(structures=caII,
-                            fixed="94+96+119",
-                            num_sequences=10,
+    ubiquitin = PDB("4LCD")
+    sequences = ProteinMPNN(structures=ubiquitin,
+                            num_sequences=50,
                             soluble_model=True)
     folded = AlphaFold(proteins=sequences)
     dna = DNAEncoder(sequences=sequences, 
                      organism="EC")
-    conf_change = ConformationalChange(reference_structures = caII,
+    conf_change = ConformationalChange(reference_structures = ubiquitin,
                                        target_structures = folded)
     filtered_sequences = Panda(tables=[folded.tables.confidence,
                                        conf_change.tables.changes],
