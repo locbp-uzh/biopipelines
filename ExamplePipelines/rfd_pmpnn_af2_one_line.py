1a6926a3547ae2f12d75281a667b8adb396609a0
GQChem <gianluca.quargnali@gmail.com>
2026-03-04 21:11:06 +0100

    autoregistration example

---
+++ ExamplePipelines/rfd_pmpnn_af2_one_line.py
@@ -0,0 +1,21 @@
+# Authors (2026): G. Quargnali & P. Rivera-Fuentes @ LOCBP (https://www.locbp.com/) University of Zurich Switzerland
+#
+# Licensed under the MIT License. See LICENSE file in the project root for details.
+
+# tested: 13.2.2026
+
+from biopipelines.pipeline import *
+from biopipelines.rfdiffusion import RFdiffusion
+from biopipelines.protein_mpnn import ProteinMPNN
+from biopipelines.alphafold import AlphaFold
+
+with Pipeline(project="Examples", job="ContextAutoregistrationDemonstration"):
+    Resources(gpu="A100", time="4:00:00", memory="16GB")
+    AlphaFold(ProteinMPNN(RFdiffusion(contigs='100-150',num_designs=10), num_sequences=2))    
+
+
+    
+
+
+    
+
